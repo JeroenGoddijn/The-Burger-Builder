c6d2be3d7209206cb1a6911ed53bb3360db7fc46
Jeroen Goddijn <jeroen.goddijn@gmail.com>
2020-12-18 16:01:15 -0600

    Implemented persistent Auth state with localStorage

--- the-burger-builder/src/containers/Auth/Auth.js
+++ the-burger-builder/src/containers/Auth/Auth.js
@@ -44,7 +44,7 @@ class Auth extends Component {
 
   componentDidMount() {
     if (!this.props.isBuildingBurger && this.props.authRedirectPath !== "/") {
-      this.props.setAuthRedirectPath();
+      this.props.onSetAuthRedirectPath();
     }
   }
 
--- the-burger-builder/src/store/actions/auth.js
+++ the-burger-builder/src/store/actions/auth.js
@@ -23,17 +23,20 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("expirationDate");
+  localStorage.removeItem("userId");
   return {
     type: actionTypes.AUTH_LOGOUT
   };
 };
 
 // checkAuthTimeout checks when the accessToken expires and then actively logs out the user
-export const checkAuthTimeout = (expirationTime) => {
+export const checkAuthTimeout = (expirationDate) => {
   return (dispatch) => {
     setTimeout(() => {
       dispatch(logout());
-    }, expirationTime * 1000);
+    }, expirationDate * 1000);
   };
 };
 
@@ -55,8 +58,18 @@ export const auth = (email, password, isSignup) => {
       .post(url, authData)
       .then((response) => {
         console.log(response);
+        const expirationDate = new Date(
+          new Date().getTime() + response.data.expiresIn * 1000
+        );
+        localStorage.setItem("accessToken", response.data.idToken);
+        localStorage.setItem("expirationDate", expirationDate);
+        localStorage.setItem("userId", response.data.localId);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
-        dispatch(checkAuthTimeout(response.data.expiresIn));
+        dispatch(
+          checkAuthTimeout(
+            expirationDate.getSeconds() - new Date().getSeconds()
+          )
+        );
       })
       .catch((err) => {
         console.log(err);
@@ -69,5 +82,22 @@ export const setAuthRedirectPath = (path) => {
   return {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path
+  };
+};
+
+export const authCheckState = () => {
+  return (dispatch) => {
+    const token = localStorage.getItem("accessToken");
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = localStorage.getItem("expirationDate");
+      if (expirationDate > new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem("userId");
+        dispatch(authSuccess(token, userId));
+      }
+    }
   };
 };
