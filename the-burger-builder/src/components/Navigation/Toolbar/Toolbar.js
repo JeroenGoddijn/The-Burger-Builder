3cd7268598bf8f654bc1604ec7ac530e891b6931
Jeroen Goddijn <jeroen.goddijn@gmail.com>
2020-12-18 13:46:42 -0600

    Updating UI based on user authentication status: LOG OUT when user is logged in, LOG IN when user is logged out

--- the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Toolbar.css';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
-import SideDrawerToggle from '../SideDrawer/SideDrawerToggle/SideDrawerToggle';
-
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import SideDrawerToggle from "../SideDrawer/SideDrawerToggle/SideDrawerToggle";
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <SideDrawerToggle clicked={props.sideDrawerToggleClicked} />
-        <div className={classes.Logo}>
-            <Logo />
-        </div>
-        <nav className={classes.DesktopOnly}>
-            <NavigationItems />
-        </nav>
-    </header>
+  <header className={classes.Toolbar}>
+    <SideDrawerToggle clicked={props.sideDrawerToggleClicked} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems isAuthenticated={props.isAuth} />
+    </nav>
+  </header>
 );
 
 export default toolbar;
--- the-burger-builder/src/hoc/Layout/Layout.js
+++ the-burger-builder/src/hoc/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Aux from "../../hoc/Aux/Aux";
 import classes from "./Layout.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
-class layout extends Component {
+class Layout extends Component {
   state = {
     showSideDrawer: true
   };
@@ -23,8 +24,12 @@ class layout extends Component {
   render() {
     return (
       <Aux>
-        <Toolbar sideDrawerToggleClicked={this.sideDrawerToggleHandler} />
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          sideDrawerToggleClicked={this.sideDrawerToggleHandler}
+        />
         <SideDrawer
+          isAuth={this.props.isAuthenticated}
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
         />
@@ -34,4 +39,9 @@ class layout extends Component {
   }
 }
 
-export default layout;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+export default connect(mapStateToProps)(Layout);
