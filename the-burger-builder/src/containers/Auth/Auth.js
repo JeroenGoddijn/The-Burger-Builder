98ca69b8c5a566da1311e27cdedcd5848557f974
Jeroen Goddijn <jeroen.goddijn@gmail.com>
2020-12-18 15:39:17 -0600

    Redirecting user to the checkout page while keeping burger-config while user authenticates (if not signed in yet)

--- the-burger-builder/src/containers/Auth/Auth.js
+++ the-burger-builder/src/containers/Auth/Auth.js
@@ -42,6 +42,12 @@ class Auth extends Component {
     }
   };
 
+  componentDidMount() {
+    if (!this.props.isBuildingBurger && this.props.authRedirectPath !== "/") {
+      this.props.setAuthRedirectPath();
+    }
+  }
+
   checkValidity(value, rules) {
     let isValid = true;
     if (!rules) {
@@ -137,7 +143,7 @@ class Auth extends Component {
 
     let authRedirect = null;
     if (this.props.isAuthenticated) {
-      authRedirect = <Redirect to="/" />;
+      authRedirect = <Redirect to={this.props.authRedirectPath} />;
     }
 
     return (
@@ -160,13 +166,17 @@ const mapStateToProps = (state) => {
   return {
     loading: state.auth.loading,
     error: state.auth.error,
-    isAuthenticated: state.auth.token !== null
+    isAuthenticated: state.auth.token !== null,
+    isBuildingBurger: state.burgerBuilder.isBuilding,
+    authRedirectPath: state.auth.authRedirectPath
   };
 };
+
 const mapDispatchToProps = (dispatch) => {
   return {
     onAuth: (email, password, isSignup) =>
-      dispatch(actions.auth(email, password, isSignup))
+      dispatch(actions.auth(email, password, isSignup)),
+    onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath("/"))
   };
 };
 
--- the-burger-builder/src/store/actions/index.js
+++ the-burger-builder/src/store/actions/index.js
@@ -4,4 +4,4 @@ export {
   initIngredients
 } from "./burgerBuilder";
 export { purchaseInit, purchaseBurger, fetchOrders } from "./order";
-export { auth, logout } from "./auth";
+export { auth, logout, setAuthRedirectPath } from "./auth";
